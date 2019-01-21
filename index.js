e0f5645e1947bdec7bcebed777320d4ed4338597
Jeffery Cosby <jcoz1968@hotmail.com>
2019-01-20 21:38:53 -0600

    response with context and request

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const Koa = require("koa");
 const app = new Koa();
 
+// add date method to context
+app.context.date = Date();
+app.context.userData = {
+    first: 'Manny',
+    last: 'Henri'
+};
+
+// response
 app.use(ctx => { 
-    ctx.body = 'Hello Manny';
+    // use state
+    ctx.state.user = `Manny Henri`;
+    // request object
+    let from = ctx.request.origin;
+    let method = ctx.request.method;
+    // print out name with date
+    // ctx.body = `Hello ${ctx.state.user} on ${ctx.date} from ${from}-${method}`;
+    // ctx.body = `Last Name: ${ctx.userData.last}`;
+    // console.log(`Last Name: ${ctx.userData.last}`);
+    ctx.response.body = ctx.userData;
 });
 
 app.listen(3001);
